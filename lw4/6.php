110de274271ef3036ae2d10871f6418441d2a3db
SBly <SBly.1@mail.ru>
2026-03-22 12:43:09 +0300

    добавила ограничение ввода номер 6

--- lw4/6.php
+++ lw4/6.php
@@ -2,20 +2,43 @@
     
 require_once 'myFunctions.php';
 
-if (isset($_POST['postfixNotation']) && $_POST['postfixNotation'] !== '') {
+function correctEnter(string $postfixNotation): bool {
+
+    $len = myLen($postfixNotation);
+    if ($len === 0) {
+        return false;
+    }
+
+    for ($i = 0; $i < $len; $i++) {
+        if ($postfixNotation[$i] === ' ' || $postfixNotation[$i] === '+' || 
+            $postfixNotation[$i] === '-' || $postfixNotation[$i] === '*') {
+        }
+        elseif ($postfixNotation[$i] < '0' || $postfixNotation[$i] > '9') {
+            return false;
+        }
+    }
+    return true;
+}
+
+if (isset($_POST['postfixNotation']) && correctEnter($_POST['postfixNotation'])) {
     $postfixNotation = $_POST['postfixNotation'];
     $notationArray = mySplit($postfixNotation, ' ');
     $tempArr = [];
+    $correct = true;
     foreach ($notationArray as $elem) {
         if ($elem === '+' || $elem === '-' || $elem === '*') {
-            if ($elem === '+') {
+            if ($elem === '+' && isset($tempArr[count($tempArr) - 2])) {
                 $tempElem = $tempArr[count($tempArr) - 2] + $tempArr[count($tempArr) - 1];
             }
-            elseif ($elem === '-') {
+            elseif ($elem === '-' && isset($tempArr[count($tempArr) - 2])) {
                 $tempElem = $tempArr[count($tempArr) - 2] - $tempArr[count($tempArr) - 1];
             }
-            else {
+            elseif (isset($tempArr[count($tempArr) - 2])) {
                 $tempElem = $tempArr[count($tempArr) - 2] * $tempArr[count($tempArr) - 1];
+            }
+            else {
+                $correct = false;
+                break;
             }
             array_splice($tempArr, count($tempArr) - 2, 2);
             $tempArr[] = $tempElem;
@@ -24,7 +47,12 @@ if (isset($_POST['postfixNotation']) && $_POST['postfixNotation'] !== '') {
             $tempArr[] = (int)$elem;
         }
     }
-    myPrintArray($tempArr);
+    if ($correct && !isset($tempArr[1])) {
+        myPrintArray($tempArr);
+    }
+    else {
+        echo 'Введённые данные некорректны';
+    }
 }
 else {
     echo 'Введённые данные некорректны';
